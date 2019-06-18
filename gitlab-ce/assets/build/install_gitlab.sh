18168398dd527397de45cbf21b43fde033d944e9
hanwh <hanwh@douzone.com>
2019-06-18 20:45:59 +0900

    fix: cp: cannot stat 'config/database.yml.mariadb': No such file or directory

--- gitlab-ce/assets/build/install_gitlab.sh
+++ gitlab-ce/assets/build/install_gitlab.sh
@@ -3,7 +3,7 @@
 cd /home/git/gitlab
 
 sudo -u git -H cp config/gitlab.yml.example config/gitlab.yml
-sudo -u git -H cp config/database.yml.mariadb config/database.yml
+sudo -u git -H cp config/database.yml.mysql config/database.yml
 sudo -u git -H cp config/resque.yml.example config/resque.yml
 
 sudo -u git -H bundle config --local build.gpgme --use-system-libraries
