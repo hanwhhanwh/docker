cc934fcf0e83d1590689bdd44a2a6ad867d7b443
Wonhee Han <hanwhhanwh@gmail.com>
2021-05-04 17:27:53 +0900

    add requirements.txt

--- python-flask/start-flask.sh
+++ python-flask/start-flask.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 cd /app/public
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 echo "Flask ENV = flask run ${FLASK_DEBUG} --host=${FLASK_HOST} --port=${FLASK_PORT}"
 python -m flask run ${FLASK_DEBUG} --host=${FLASK_HOST} --port=${FLASK_PORT}
